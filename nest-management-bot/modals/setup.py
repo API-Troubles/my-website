aec98eb3d9c2ba9f31e198467b9ebadbbd370ae9
felixgao-0 <76596162+felixgao-0@users.noreply.github.com>
2024-11-03 10:51:17 -0500

    someone cure my stupidy, change app home to modal type

--- nest-management-bot/modals/setup.py
+++ nest-management-bot/modals/setup.py
@@ -1,4 +1,4 @@
-def setup_wizard_modal(user_token):
+def setup_token_wizard_modal(user_token):
     return {
         "type": "modal",
         "title": {
@@ -16,7 +16,7 @@ def setup_wizard_modal(user_token):
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": f"This is your client token:\n `{user_token}`"
+                    "text": f"This is your client token:\n`{user_token}`"
                 }
             },
             {
@@ -28,6 +28,64 @@ def setup_wizard_modal(user_token):
                     "type": "mrkdwn",
                     "text": f"To add the client key to your Nest user, run the following in Nest:\n```echo CLIENT_TOKEN={user_token} > ~/.env.nest-management-bot```"
                 }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": ":siren-real::warning: *Do not share your token pwease* :thumbsup-dino:"
+                }
+            }
+        ]
+    }
+
+def manage_token_wizard_modal(user_token):
+    return {
+        "type": "modal",
+        "title": {
+            "type": "plain_text",
+            "text": "My App",
+            "emoji": True
+        },
+        "close": {
+            "type": "plain_text",
+            "text": "Ok tysm!",
+            "emoji": True
+        },
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"This is your client token:\n`{user_token}`"
+                }
+            },
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"To add the client key to your Nest user, run the following in Nest:\n```echo CLIENT_TOKEN={user_token} ~/.env.nest-management-bot```"
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Reset your token",
+                            "emoji": True
+                        },
+                        "action_id": "setup-new-client-token"
+                    }
+                ]
             },
             {
                 "type": "divider"
