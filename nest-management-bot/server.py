c952b617acc318dfd133ee8396cbf75825f679db
felixgao-0 <76596162+felixgao-0@users.noreply.github.com>
2024-12-05 23:11:35 -0500

    Close websocket on error, remove client from client list properly

--- nest-management-bot/server.py
+++ nest-management-bot/server.py
@@ -9,7 +9,7 @@ import websockets
 import server_utils as utils
 from server_utils import clients
 
-
+ws_clients = {} # Internal list used to manage connection state
 db = None
 
 async def server(websocket):
@@ -24,6 +24,7 @@ async def server(websocket):
                 'message': 'The client did not send a response to the server.',
                 'status': 'error'
             }))
+            await websocket.close()
             return
 
         try:
@@ -33,22 +34,26 @@ async def server(websocket):
                 'message': 'The client sent malformed data which isn\'t JSON.',
                 'status': 'error'
             }))
+            await websocket.close()
             return
 
         # not ideal location but for some reason intellij ONLY WANTS IT *HERE*
         client_token_provided = first_message.get('payload', {}).get('client_token')
         if first_message.get('status') != "let_me_in_pls":
             await websocket.send(json.dumps({
-                'message': 'The client sent malformed data with the incorrect type.',
+                'message': 'The client sent malformed data with the incorrect type. This shouldn\'t happen, ask in #nest-management-bot.',
                 'status': 'error'
             }))
+            await websocket.close()
             return
 
         elif not db.get_user(token=client_token_provided):
             await websocket.send(json.dumps({
-                'message': 'Invalid client token or the client sent malformed data.',
+                'message': f'Invalid client token "{client_token_provided}" or the client sent malformed data.',
                 'status': 'error'
             }))
+            print(first_message_json)
+            await websocket.close()
             return
 
         elif client_token_provided in clients.keys():
@@ -56,6 +61,7 @@ async def server(websocket):
                 'message': 'Already connected, if this is incorrect regenerate your token NOW.',
                 'status': 'error'
             }))
+            await websocket.close()
             return
 
         elif first_message.get('payload', {}).get('version') != "0.1.0a":
@@ -63,6 +69,7 @@ async def server(websocket):
                 'message': 'Your version is out of date or the client sent malformed data.',
                 'status': 'error'
             }))
+            await websocket.close()
             return
 
         # User authenticated yay
@@ -70,6 +77,7 @@ async def server(websocket):
 
         # Add client to list to use for sending messages later
         clients[client_token_provided]: websocket = websocket
+        ws_clients[websocket.id]: str = client_token_provided
         print("ACTIVE CLIENTS:", clients.keys())
 
 
@@ -80,10 +88,14 @@ async def server(websocket):
                 return
 
     except websockets.exceptions.ConnectionClosed as e:
-        print(f"Connection closed: {e}")
+        print(f"Connection suddenly closed: {e}")
+    except asyncio.TimeoutError:
+        print("Client disappeared, closing connection")
+        await websocket.close()
     finally:
-        if clients.get(websocket.id):
-            clients.pop(f'{websocket.id}')
+        client_id = ws_clients.get(websocket.id)
+        if clients.get(client_id):
+            clients.pop(client_id)
         print("ACTIVE CLIENTS:", clients.keys())
 
 
@@ -91,7 +103,7 @@ async def main():
     #ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     #ssl_context.load_cert_chain("cert.pem", "private_key.pem")
 
-    async with serve(server, "localhost", 8989):#, ssl=ssl_context):
+    async with serve(server, "localhost", 8989, ping_interval=20, ping_timeout=10):#, ssl=ssl_context):
         print("server running...")
         await asyncio.get_running_loop().create_future()  # run forever
 
