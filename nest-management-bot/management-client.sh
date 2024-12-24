53331a5e5dfe8f3b547a69b6e6b28c0dff421fcf
felixgao-0 <76596162+felixgao-0@users.noreply.github.com>
2024-12-23 22:17:15 -0500

    (wip) setup bash script label what i wanna add

--- nest-management-bot/management-client.sh
+++ nest-management-bot/management-client.sh
@@ -4,7 +4,10 @@
 # If client.py doesn't exist, this script will setup the file
 # This script also keeps client.py updated with the latest version from the repository
 
-# Configuration variables :D (Don't modify these)
+# Written and maintained by Felix Gao - proudly open source :)
+# Check out the repo at https://github.com/felixgao-0/my-website
+
+# Configuration variables :D (Don't modify these unless smth changes important)
 # TODO: REMOVE DEV BRANCH AND REPLACE WITH MAIN BRANCH
 FILE_REMOTE_URL="https://raw.githubusercontent.com/felixgao-0/my-website/refs/heads/feat/nest-management-bot/nest-management-bot/client.py"
 CLIENT_FILEPATH="$HOME/client.py" # FIXME: replace with $home in prod i beg you :plead-blobhaj-1:
@@ -12,6 +15,10 @@ CLIENT_FILEPATH="$HOME/client.py" # FIXME: replace with $home in prod i beg you 
 
 remote_file_hash=$(curl -s $FILE_REMOTE_URL | sha256sum)
 
+
+# write logic here for missing the env file
+
+# Write logic here for not up to date setup file (wait thats this file!)
 
 if [ -f client.py ]; then # Check if the client file exists
     if [ "$(sha256sum "$CLIENT_FILEPATH")" == "$remote_file_hash" ]; then
@@ -25,5 +32,9 @@ else # Client doesn't exist, make it exist :D
     curl -o "$CLIENT_FILEPATH" "$FILE_REMOTE_URL"
 fi
 
+
+# write logic here for missing venv and installing it + needed dependencies
+
 echo "Running client.py"
-# insert client running here
+# Run the client with the env
+nest-management-bot/venv/bin/python3.11 nest-management-bot/client.py
