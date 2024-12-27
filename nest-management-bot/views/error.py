bea40e53826a8b3a9841d838b5c5376f47152693
felixgao-0 <76596162+felixgao-0@users.noreply.github.com>
2024-12-26 19:12:09 -0500

    make error less scary

--- nest-management-bot/views/error.py
+++ nest-management-bot/views/error.py
@@ -31,7 +31,7 @@ async def generate_error(client, user_id, error: str):
                     "type": "section",
                     "text": {
                         "type": "mrkdwn",
-                        "text": "_If the error does not contain troubleshooting steps, take a screenshot of this error and send it in #nest-management-bot :heavysob: I'm sorry (Canadian Edition apology)_"
+                        "text": "_Usually these don't mean much (I likely was too lazy to code a proper menu for error handling). If the error does not contain troubleshooting steps and the error sounds scary, take a screenshot of this error and send it in #nest-management-bot :heavysob: I'm sorry (Canadian Edition apology)_"
                     }
                 },
                 {
