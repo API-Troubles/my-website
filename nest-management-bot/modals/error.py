be03aca833a425873564190dcb91a735a972f6fa
felixgao-0 <76596162+felixgao-0@users.noreply.github.com>
2024-12-26 20:04:18 -0500

    better error messaging

--- nest-management-bot/modals/error.py
+++ nest-management-bot/modals/error.py
@@ -44,7 +44,7 @@ def error_modal(error):
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": "_If the error does not contain troubleshooting steps, take a screenshot of this error and send it in #nest-management-bot :heavysob: I'm sorry (Canadian Edition apology)_"
+                    "text": "_Usually these don't mean much (I likely was too lazy to code a proper menu for error handling). If the error does not contain troubleshooting steps, take a screenshot of this error and send it in #nest-management-bot :heavysob: I'm sorry (Canadian Edition apology)_"
                 }
             },
             {
