f279ccb056d64fda1519039f17591118c73f7b5f
felixgao-0 <76596162+felixgao-0@users.noreply.github.com>
2024-12-31 21:34:11 -0500

    fix systemd configuration, add colours :D

--- nest-management-bot/management-client.sh
+++ nest-management-bot/management-client.sh
@@ -25,9 +25,22 @@ client_remote_hash=$(curl -s CLIENT_REMOTE_URL | sha256sum)
 service_remote_hash=$(curl -s SERVICE_REMOTE_URL | sha256sum)
 
 
+colour_echo () {
+    # Syntax: colour_echo <message> <colour>
+    if [ "$2" == "red" ]; then
+        # I shoved error handling here cause why else use red? (famous last words)
+        echo -e "\033[31m{$1}\033[0m" >&2
+        exit 1
+    elif [ "$2" == "green" ]; then
+        echo -e "\033[32m{$1}\033[0m"
+    elif [ "$2" == "yellow" ]; then
+        echo -e "\033[33m{$1}\033[0m"
+    fi
+}
+
+
 if [ ! -f "./.env.nest-management-bot" ]; then
-    echo "Before you run this setup script make sure you run the token setup script" >&2
-    exit 1
+    colour_echo "Before you run this setup script make sure you run the token setup script" "red"
 fi
 
 if [ ! -d "./nest-management-bot" ]; then
@@ -38,17 +51,21 @@ fi
 
 if [ -f SERVICE_FILEPATH ]; then
     if [ "$(sha256sum "$SERVICE_FILEPATH")" == "$service_remote_hash" ]; then
-            echo "Systemd service file exists and is up to date"
-        else
-            echo "Systemd service file is not up to date, updating nest-management-bot.service"
-            curl -o "$SERVICE_FILEPATH" "$SERVICE_REMOTE_URL"
-            systemctl --user daemon-reload
-        fi
+        echo "Systemd service file exists and is up to date"
+    else
+        echo "Systemd service file is not up to date, updating nest-management-bot.service"
+        curl -o "$SERVICE_FILEPATH" "$SERVICE_REMOTE_URL"
+        systemctl --user daemon-reload
+    fi
 else
     echo "Systemd service file doesn't exist, creating nest-management-bot.service"
     curl -o "$SERVICE_FILEPATH" "$SERVICE_REMOTE_URL"
     systemctl --user daemon-reload
     systemctl --user enable nest-management-bot.service
+    systemctl --user start nest-management-bot.service
+    echo "Exiting as the service file was created... bot is running from the service file now"
+    colour_echo "Hint: Check systemctl --user status nest-management-bot.service\u001b" "yellow"
+    exit 0
 fi
 
 
@@ -76,5 +93,5 @@ else
 fi
 
 
-echo "Running client.py"
-nest-management-bot/venv/bin/python3.11 nest-management-bot/client.py
+colour_echo "Running client.py... finally!" "green"
+nest-management-bot/venv/bin/python3.11 nest-management-bot/client.py &
